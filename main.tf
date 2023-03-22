2d9291275d0a9de31c3c3ac983fa467c4fdb5cf8
soyooniii <ksoyooniii@gmaill.com>
2023-03-22 15:52:08 +0900

    4th main.tf commit

--- main.tf
+++ main.tf
@@ -73,28 +73,35 @@ resource "aws_security_group" "my-project-public-sg" {
   name_prefix = "my-project-public-sg"
   vpc_id      = aws_vpc.my-project-vpc.id
 
-ingress {
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+  ingress {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
 
-ingress {
+  ingress {
     from_port   = 30080
     to_port     = 30080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
 
-ingress {
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
 
-ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
